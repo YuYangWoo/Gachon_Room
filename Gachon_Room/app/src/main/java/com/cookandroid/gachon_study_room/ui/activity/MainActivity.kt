b194be88293dd75f7ab773eacabca1f724893d16
yuyw0712 <yuyw0712@naver.com>
2021-02-04 14:17:23 +0900

    스플래시 화면 구현

--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/activity/MainActivity.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/activity/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun init() {
         super.init()
-
+        splash()
         initToolbar()
     }
 
--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/base/BaseActivity.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/base/BaseActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.cookandroid.gachon_study_room.R
 
 abstract class BaseActivity<VB: ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
     protected lateinit var binding: VB
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        splash()
         super.onCreate(savedInstanceState)
         init()
     }
 
     protected open fun init() {
         initViewDataBinding()
+    }
+
+    protected open fun splash() {
+      setTheme(R.style.Theme_Gachon_Study_Room)
     }
 
     protected open fun initViewDataBinding() {
