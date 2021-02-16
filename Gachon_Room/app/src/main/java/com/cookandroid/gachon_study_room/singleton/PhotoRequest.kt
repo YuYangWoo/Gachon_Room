d275095f3b7d55c188ce4c68bf4530c42516af02
yuyw0712 <yuyw0712@naver.com>
2021-02-16 15:58:28 +0900

    MainFragment CardView 이름, 학번, 과 구현

--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/singleton/PhotoRequest.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/singleton/PhotoRequest.kt
@@ -1,73 +1,27 @@
-package com.cookandroid.gachon_study_room.singleton
-
-import android.content.Context
-import android.util.Log
-import androidx.core.content.ContentProviderCompat.requireContext
-import com.bumptech.glide.Glide
-import com.cookandroid.gachon_study_room.ui.fragment.MainFragment
-import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.net.HttpURLConnection
-import java.net.URL
-
-class PhotoRequest(context: Context) {
-    private lateinit var login: Job
- //   private lateinit var session: Job
-    private val id by lazy { MySharedPreferences.getUserId(context) }
-    private val pw by lazy { MySharedPreferences.getUserPass(context) }
-
-    fun requestLogin() = runBlocking {
-        login = CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val url = URL("https://sso.gachon.ac.kr/svc/tk/Login.do")
-                var urlConn = url.openConnection() as HttpURLConnection
-
-                // urlConn 설정
-                urlConn.requestMethod = "POST"
-                urlConn.setRequestProperty("Accept-Charset", "UTF-8")
-                urlConn.setRequestProperty(
-                        "Content_Type",
-                        "application/x-www-form-urlencoded"
-                )
-                urlConn.doOutput = true
-
-                val osw = OutputStreamWriter(urlConn.outputStream)
-                var sendMsg = "user_id=$id&user_password=$pw&id=www"
-                osw.write(sendMsg)
-                osw.flush()
-
-                if (urlConn.responseCode == HttpURLConnection.HTTP_OK) {
-                    Log.d("test", "login 연결성공")
-
-                    val streamReader = InputStreamReader(urlConn.inputStream)
-                    val buffered = BufferedReader(streamReader)
-
-                    val content = StringBuilder()
-                    while(true) {
-                        val line = buffered.readLine() ?: break
-                        content.append(line)
-                    }
-
-                    Log.d("test", "content $content")
-                    // 스트림과 커넥션 해제
-//                    Log.d("test", "receiveMsg $receiveMsg")
-                    buffered.close()
-
-                } else {    // 통신이 실패한 이유를 찍기위한 로그
-                    Log.d("통신 결과", "에러" + urlConn.responseCode)
-                }
-            } catch (e: Exception) {
-                Log.d("error", e.printStackTrace().toString() + "dd")
-            }
-        }
-        login.join()
-        Log.d("test", "login 후")
-//        session = CoroutineScope(Dispatchers.IO).launch {
+//package com.cookandroid.gachon_study_room.singleton
 //
+//import android.content.Context
+//import android.util.Log
+//import androidx.core.content.ContentProviderCompat.requireContext
+//import com.bumptech.glide.Glide
+//import com.cookandroid.gachon_study_room.ui.fragment.MainFragment
+//import kotlinx.coroutines.*
+//import java.io.BufferedReader
+//import java.io.InputStreamReader
+//import java.io.OutputStreamWriter
+//import java.net.HttpURLConnection
+//import java.net.URL
+//
+//class PhotoRequest(context: Context) {
+//    private lateinit var login: Job
+// //   private lateinit var session: Job
+//    private val id by lazy { MySharedPreferences.getUserId(context) }
+//    private val pw by lazy { MySharedPreferences.getUserPass(context) }
+//
+//    fun requestLogin() = runBlocking {
+//        login = CoroutineScope(Dispatchers.IO).launch {
 //            try {
-//                val url = URL("https://info.gachon.ac.kr/exsignon/sso/sso_index.jsp")
+//                val url = URL("https://sso.gachon.ac.kr/svc/tk/Login.do")
 //                var urlConn = url.openConnection() as HttpURLConnection
 //
 //                // urlConn 설정
@@ -77,8 +31,16 @@ class PhotoRequest(context: Context) {
 //                        "Content_Type",
 //                        "application/x-www-form-urlencoded"
 //                )
+//                urlConn.doOutput = true
+//
+//                val osw = OutputStreamWriter(urlConn.outputStream)
+//                var sendMsg = "user_id=$id&user_password=$pw&id=www"
+//                osw.write(sendMsg)
+//                osw.flush()
+//
 //                if (urlConn.responseCode == HttpURLConnection.HTTP_OK) {
-//                    Log.d("test", "session 연결성공")
+//                    Log.d("test", "login 연결성공")
+//
 //                    val streamReader = InputStreamReader(urlConn.inputStream)
 //                    val buffered = BufferedReader(streamReader)
 //
@@ -89,20 +51,58 @@ class PhotoRequest(context: Context) {
 //                    }
 //
 //                    Log.d("test", "content $content")
-//
 //                    // 스트림과 커넥션 해제
+////                    Log.d("test", "receiveMsg $receiveMsg")
 //                    buffered.close()
-//                    urlConn.disconnect()
 //
 //                } else {    // 통신이 실패한 이유를 찍기위한 로그
 //                    Log.d("통신 결과", "에러" + urlConn.responseCode)
 //                }
 //            } catch (e: Exception) {
-//                Log.d("test", e.printStackTrace().toString() + "session")
+//                Log.d("error", e.printStackTrace().toString() + "dd")
 //            }
 //        }
-//        session.join()
-
-    }
-
-}
+//        login.join()
+//        Log.d("test", "login 후")
+////        session = CoroutineScope(Dispatchers.IO).launch {
+////
+////            try {
+////                val url = URL("https://info.gachon.ac.kr/exsignon/sso/sso_index.jsp")
+////                var urlConn = url.openConnection() as HttpURLConnection
+////
+////                // urlConn 설정
+////                urlConn.requestMethod = "POST"
+////                urlConn.setRequestProperty("Accept-Charset", "UTF-8")
+////                urlConn.setRequestProperty(
+////                        "Content_Type",
+////                        "application/x-www-form-urlencoded"
+////                )
+////                if (urlConn.responseCode == HttpURLConnection.HTTP_OK) {
+////                    Log.d("test", "session 연결성공")
+////                    val streamReader = InputStreamReader(urlConn.inputStream)
+////                    val buffered = BufferedReader(streamReader)
+////
+////                    val content = StringBuilder()
+////                    while(true) {
+////                        val line = buffered.readLine() ?: break
+////                        content.append(line)
+////                    }
+////
+////                    Log.d("test", "content $content")
+////
+////                    // 스트림과 커넥션 해제
+////                    buffered.close()
+////                    urlConn.disconnect()
+////
+////                } else {    // 통신이 실패한 이유를 찍기위한 로그
+////                    Log.d("통신 결과", "에러" + urlConn.responseCode)
+////                }
+////            } catch (e: Exception) {
+////                Log.d("test", e.printStackTrace().toString() + "session")
+////            }
+////        }
+////        session.join()
+//
+//    }
+//
+//}
--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/fragment/MainFragment.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/fragment/MainFragment.kt
@@ -4,37 +4,37 @@ import android.content.Intent
 import android.util.Log
 import com.bumptech.glide.Glide
 import com.cookandroid.gachon_study_room.R
+import com.cookandroid.gachon_study_room.data.StudentInformation
 import com.cookandroid.gachon_study_room.databinding.FragmentMainBinding
 import com.cookandroid.gachon_study_room.singleton.MySharedPreferences
-import com.cookandroid.gachon_study_room.singleton.PhotoRequest
-import com.cookandroid.gachon_study_room.singleton.SessionRequest
 import com.cookandroid.gachon_study_room.ui.activity.LoginActivity
 import com.cookandroid.gachon_study_room.ui.base.BaseFragment
 import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
-    private lateinit var login: Job
-    private lateinit var job: Job
+//    private lateinit var login: Job
+//    private lateinit var job: Job
+    private val info: StudentInformation by lazy {
+        MySharedPreferences.getInformation(requireContext())
+    }
+
     override fun init() {
         super.init()
         Log.d("test", MySharedPreferences.getUserId(requireContext()))
         logout()
-        http()
+
+       binding.student = info
     }
 
-    private fun http() = runBlocking {
-       login = CoroutineScope(Dispatchers.IO).launch { PhotoRequest(requireContext()).requestLogin() }
-        login.join()
-        job = CoroutineScope(Dispatchers.Default).launch{SessionRequest().request()}
-        job.join()
-        Glide.with(requireContext()).load("https://info.gachon.ac.kr/StuCommonInfo/doGetPhoto.do?CALL_PAGE=STASTA_SHJSHJ09shj0901e&STUDENT_NO=201636010&p=826").into(binding.img)
+//    private fun http() = runBlocking {
+//       login = CoroutineScope(Dispatchers.IO).launch { PhotoRequest(requireContext()).requestLogin() }
+//        login.join()
+//        job = CoroutineScope(Dispatchers.Default).launch{SessionRequest().request()}
+//        job.join()
+//        Glide.with(requireContext()).load("https://info.gachon.ac.kr/StuCommonInfo/doGetPhoto.do?CALL_PAGE=STASTA_SHJSHJ09shj0901e&STUDENT_NO=201636010&p=826").into(binding.img)
+//
+//    }
 
-    }
 
     private fun logout() {
         binding.btnLogout.setOnClickListener {
