28a6087a24b406d4ce676eaf40d4d784ae2a4080
yuyw0712 <yuyw0712@naver.com>
2021-02-17 16:03:56 +0900

    QR Reader 추가 및 코드리팩토링

---
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/activity/CaptureForm.kt
@@ -0,0 +1,11 @@
+package com.cookandroid.gachon_study_room.ui.activity
+
+import android.os.Bundle
+import com.journeyapps.barcodescanner.CaptureActivity
+
+
+class CaptureForm : CaptureActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
