857b5dcf429aea4674f4d8a4dbda90d9561bd3ea
YuYangWoo <59405161+YuYangWoo@users.noreply.github.com>
2021-03-24 18:05:56 +0900

    MySeat API 통신 후 txt Bind

--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/service/API.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/service/API.kt
@@ -1,6 +1,7 @@
 package com.cookandroid.gachon_study_room.service
 
 import com.cookandroid.gachon_study_room.data.Information
+import com.cookandroid.gachon_study_room.data.MySeat
 import com.cookandroid.gachon_study_room.data.room.RoomsData
 import retrofit2.Call
 import retrofit2.http.FieldMap
@@ -15,4 +16,8 @@ interface API {
     @FormUrlEncoded
     @POST("/login")
     fun loginRequest(@FieldMap param: HashMap<String, String>): Call<Information>
+
+    @FormUrlEncoded
+    @POST("/room/myReservation")
+    fun mySeatRequest(@FieldMap param: HashMap<String, String>): Call<MySeat>
 }
--- Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/dialog/MySeatDialog.kt
+++ Gachon_Room/app/src/main/java/com/cookandroid/gachon_study_room/ui/dialog/MySeatDialog.kt
@@ -1,13 +1,39 @@
 package com.cookandroid.gachon_study_room.ui.dialog
 
+import android.util.Log
 import com.cookandroid.gachon_study_room.R
+import com.cookandroid.gachon_study_room.data.MySeat
 import com.cookandroid.gachon_study_room.databinding.FragmentMySeatBinding
+import com.cookandroid.gachon_study_room.service.RetrofitBuilder
+import com.cookandroid.gachon_study_room.singleton.MySharedPreferences
 import com.cookandroid.gachon_study_room.ui.base.BaseBottomSheet
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MySeatDialog : BaseBottomSheet<FragmentMySeatBinding>(R.layout.fragment_my_seat) {
     override fun init() {
         super.init()
 
+        var input = HashMap<String, String>()
+        input["id"] = MySharedPreferences.getUserId(requireContext())
+        input["password"] = MySharedPreferences.getUserPass(requireContext())
+        RetrofitBuilder.api.mySeatRequest(input).enqueue(object : Callback<MySeat> {
+            override fun onResponse(call: Call<MySeat>, response: Response<MySeat>) {
+                if(response.isSuccessful) {
+                    var mySeatData = response.body()!!
+                    Log.d("TAG", mySeatData.toString())
+                    binding.txtNumber.text = mySeatData.reservations[0].studentId
+                    binding.txtLocation.text = mySeatData.reservations[0].college + mySeatData.reservations[0].room
+                    binding.txtTime.text = "${mySeatData.reservations[0].begin} ~ ${mySeatData.reservations[0].end}"
+                }
+            }
+
+            override fun onFailure(call: Call<MySeat>, t: Throwable) {
+                Log.d("Error", "mySeat 연결 실패 $t")
+            }
+
+        })
     }
 }
